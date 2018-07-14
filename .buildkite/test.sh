f78c23754db63b7861052927a117fa33955847e5
Randy Fay <randy@randyfay.com>
2018-07-14 17:44:07 -0600

    Improve the yml and test.sh

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
 
-# This script is used to build drud/ddev using buildkite
+# This script is used to build drud/build-tools using buildkite
 
 # Manufacture a $GOPATH environment that can mount on docker (when buildkite build)
-if [ ! -z "$BUILDKITE_JOB_ID" ]; then
-	export GOPATH=~/tmp/buildkite/$BUILDKITE_JOB_ID
-	DRUDSRC=$GOPATH/src/github.com/drud
-	mkdir -p $DRUDSRC
-	ln -s $PWD $DRUDSRC/ddev
-	cd $DRUDSRC/ddev
-	echo "--- buildkite building $BUILDKITE_JOB_ID at $(date) on $(hostname) for OS=$(go env GOOS) in $DRUDSRC/ddev"
-fi
+export GOPATH=~/tmp/buildkite-fake-gopath/$BUILDKITE_JOB_ID
+DRUDSRC=$GOPATH/src/github.com/drud
+mkdir -p $DRUDSRC
+ln -s $PWD $DRUDSRC/build-tools
+cd $DRUDSRC/build-tools
+BUILD_OS=$(go env GOOS)
+echo "--- buildkite building $BUILDKITE_JOB_ID at $(date) on $HOSTNAME for OS=$(go env GOOS) in $PWD GOPATH=$GOPATH"
 
-export GOTEST_SHORT=1
-
-echo "--- cleaning up docker and Test directories"
-echo "Warning: deleting all docker containers and deleting ~/.ddev/Test*"
+echo "--- cleaning up docker"
+echo "Warning: deleting all docker containers"
 if [ "$(docker ps -aq | wc -l)" -gt 0 ] ; then
 	docker rm -f $(docker ps -aq)
 fi
-docker system prune --volumes --force
+docker system prune --force
 
 # Update all images that could have changed
 docker images | awk '/drud/ {print $1":"$2 }' | xargs -L1 docker pull
-rm -rf ~/.ddev/Test*
 
 set -o errexit
 set -o pipefail
@@ -33,8 +29,11 @@ set -x
 # Our testbot should now be sane, run the testbot checker to make sure.
 ./.buildkite/sanetestbot.sh
 
-echo "Running tests..."
-cd tests && time make test
+echo "--- make $BUILD_OS"
+cd tests
+time make $BUILD_OS
+echo "--- make test"
+time make test
 RV=$?
-echo "build.sh completed with status=$RV"
+echo "--- build.sh completed with status=$RV"
 exit $RV
