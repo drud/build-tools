5ae1c52290641eb8f1a3734bbbcc9ca74251d5dd
Randy Fay <randy@randyfay.com>
2019-01-08 15:16:24 -0700

    Golang v1.11 module support for build-tools (#60)
    * Bump to golang v1.11.4
    * Convert to go modules
    * Add the with_vendor directory to the test
    * Update tests for go 1.11 behavior
    * Use -mod=vendor if vendor directory exists

--- tests/pkg/clean/build_tools_test.go
+++ tests/pkg/clean/build_tools_test.go
@@ -34,9 +34,9 @@ func TestBuild(t *testing.T) {
 
 	// Map OS name to output location
 	binlocs := map[string]string{
-		"darwin":  "bin/darwin/darwin_amd64/build_tools_dummy",
-		"linux":   "bin/linux/build_tools_dummy",
-		"windows": "bin/windows/windows_amd64/build_tools_dummy.exe",
+		"darwin":  ".gotmp/bin/darwin_amd64/build_tools_dummy",
+		"linux":   ".gotmp/bin/build_tools_dummy",
+		"windows": ".gotmp/bin/windows_amd64/build_tools_dummy.exe",
 	}
 
 	dir, _ := os.Getwd()
@@ -75,7 +75,6 @@ func TestBuild(t *testing.T) {
 	a.NoError(err)
 	a.Contains(string(v), "This is build_tools_dummy.go")
 	a.Contains(string(v), version.VERSION)
-	a.Contains(string(v), version.COMMIT)
 	a.Contains(string(v), "Built ")
 	a.NotContains(string(v), "COMMIT should be overridden")
 	a.NotContains(string(v), "BUILDINFO should have new info")
@@ -99,53 +98,6 @@ func TestGoFmt(t *testing.T) {
 	// Test "make SRC_DIRS=pkg/clean gofmt" - has no errors
 	v, err = exec.Command("make", "SRC_DIRS=pkg/clean", "gofmt").Output()
 	a.NoError(err, "make SRC_DIRS=pkg/clean gofmt failed: output="+string(v)) // No error on the clean directory
-
-}
-
-// Use govendor with extra and missing items
-// There is some danger here that failure can leave the vendor directory with changed items.
-// Note that the spare make target "make container_cmd" is used as a generic way to execute govendor in the container.
-// The COMMAND=some command argument to exec.Command() is an oddity - due to the way this argument is processed,
-// it must not be escaped.
-func TestGovendor(t *testing.T) {
-	a := assert.New(t)
-
-	simpleExtraPackage := "golang.org/x/net/context"
-	neededPackage := "github.com/stretchr/testify/assert"
-
-	// Test "make govendor"
-	_, err := exec.Command("make", "govendor").Output()
-	a.NoError(err) // Base code should have no errors
-
-	// Add an unused vendor item (net/context) and check that our govendor now fails
-	v, err := exec.Command("make", "COMMAND=govendor fetch "+simpleExtraPackage, "container_cmd").Output()
-	a.NoError(err, "Failed 'govendor fetch %v', result=%v", simpleExtraPackage, string(v))
-
-	v, err = exec.Command("make", "govendor").Output()
-	a.Error(err) // We should have an error now, with unused item
-	a.Contains(string(v), "u "+simpleExtraPackage)
-
-	// Remove the extra item
-	_, err = exec.Command("make", "COMMAND=govendor remove "+simpleExtraPackage, "container_cmd").Output()
-	a.NoError(err)
-	// Test "make govendor" - should be back to no errors
-	_, err = exec.Command("make", "govendor").Output()
-	a.NoError(err) // Base code should have no errors
-
-	// Remove a necessary package
-	_, err = exec.Command("make", "COMMAND=govendor remove "+neededPackage, "container_cmd").Output()
-	a.NoError(err)
-	// Test "make govendor" - should show assert as a missing item
-	v, err = exec.Command("make", "govendor").Output()
-	a.Error(err)
-	a.Contains(string(v), "m "+neededPackage)
-
-	_, err = exec.Command("make", "COMMAND=govendor fetch "+neededPackage, "container_cmd").Output()
-	a.NoError(err)
-
-	// Try to clean up the mess we may have made in the vendor directory
-	_, _ = exec.Command("git", "checkout vendor").Output()
-	_, _ = exec.Command("git", "clean -fd vendor").Output()
 
 }
 
@@ -180,6 +132,7 @@ func TestGoVet(t *testing.T) {
 
 // Test errcheck.
 func TestErrCheck(t *testing.T) {
+	t.Skip("errcheck does not yet work with go 1.11 modules, see https://github.com/kisielk/errcheck/issues/155")
 	a := assert.New(t)
 
 	// pkg/dirtycomplex/bad_errcheck_code.go
@@ -207,35 +160,6 @@ func TestStaticcheck(t *testing.T) {
 	a.NoError(err) // Should have no complaints in clean package
 }
 
-// Test unused.
-func TestUnused(t *testing.T) {
-	a := assert.New(t)
-
-	// Test "make unused"
-	v, err := exec.Command("make", "unused").Output()
-	a.Error(err) // Should have one complaint about bad_unused_code.go
-	a.Contains(string(v), "pkg/dirtyComplex/bad_unused_code.go")
-
-	// Test "make SRC_DIRS=pkg/clean unused" to limit to just clean directories
-	_, err = exec.Command("make", "unused", "SRC_DIRS=pkg/clean").Output()
-	a.NoError(err) // Should have no complaints in clean package
-}
-
-// Test codecoroner.
-func TestCodeCoroner(t *testing.T) {
-	a := assert.New(t)
-
-	// Test "make codecoroner"
-	v, err := exec.Command("make", "codecoroner").Output()
-	a.Error(err)                                        // Should complain about pretty much everything in the dirtyComplex package.
-	a.Contains(string(v), "AnotherExportedFunction")    // Check an exported function
-	a.Contains(string(v), "yetAnotherExportedFunction") // Check an unexported function.
-
-	// Test "make SRC_DIRS=pkg/clean codecoroner" to limit to just clean directories
-	_, err = exec.Command("make", "codecoroner", "SRC_DIRS=pkg/clean").Output()
-	a.NoError(err) // Should have no complaints in clean package
-}
-
 // Test misspell.
 func TestMisspell(t *testing.T) {
 	a := assert.New(t)
@@ -245,10 +169,10 @@ func TestMisspell(t *testing.T) {
 	a.NoError(err)                                               // This one doesn't make an error return
 	a.Contains(string(v), " is a misspelling of \"misspelled\"") // Check an exported function
 
-	// Test "make SRC_DIRS=pkg/clean codecoroner" to limit to just clean directories
+	// Test "make SRC_DIRS=pkg/clean misspell" to limit to just clean directories
 	v, err = exec.Command("make", "--no-print-directory", "misspell", "SRC_DIRS=pkg/clean").Output()
 	a.NoError(err) // Should have no complaints in clean package
-	a.Equal(string(v), "Checking for misspellings: \n")
+	a.Equal("Checking for misspellings: \n", string(v))
 }
 
 // Test gometalinter.
@@ -274,7 +198,7 @@ func TestGolangciLint(t *testing.T) {
 		t.Skip("Skipping TestGolangciLint on Windows; golangci-lint fails with dockertoolbox, see https://github.com/golangci/golangci-worker/blob/caca2738602c324b1a1d6633ad959aa6d883f2df/app/analyze/executors/temp_dir_shell.go#L27")
 	}
 
-	// Test "make gometalinter"
+	// Test "make golangci-lint"
 	v, err := exec.Command("make", "golangci-lint").Output()
 	a.Error(err) // Should complain about pretty much everything in the dirtyComplex package.
 	execResult := string(v)
--- tests/pkg/clean/clean.go
+++ tests/pkg/clean/clean.go
@@ -1,3 +1,5 @@
 package clean
 
+// this donnot work
+
 // This file has no gofmt or other errors
