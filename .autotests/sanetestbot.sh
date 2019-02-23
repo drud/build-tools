08d201b3f3987692f8c40f25083e845e2ff46c67
Randy Fay <randy@randyfay.com>
2019-02-23 08:22:27 -0800

    Remove mysql-cli requirement

--- .autotests/sanetestbot.sh
+++ .autotests/sanetestbot.sh
@@ -9,7 +9,7 @@ set -o nounset
 
 
 # Check that required commands are available.
-for command in mysql git go make; do
+for command in git go make; do
     command -v $command >/dev/null || ( echo "Did not find command installed '$command'" && exit 2 )
 done
 docker run -t busybox ls >/dev/null
